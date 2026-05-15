e68a3e833dba421d5c4f8812daf0bee9b89b3488
Naiyuan Qing <145280634+NevilleQingNY@users.noreply.github.com>
2026-05-15 18:33:18 -0400

    chore: add statica-sdk package and GitHub Packages publish workflow

---
+++ packages/sdk/index.js
@@ -0,0 +1,10 @@
+/**
+ * statica-sdk
+ * JavaScript SDK for the Statica managed agents platform.
+ * https://github.com/statica-ai/statica
+ */
+
+module.exports = {
+  name: 'statica-sdk',
+  version: require('./package.json').version,
+};
